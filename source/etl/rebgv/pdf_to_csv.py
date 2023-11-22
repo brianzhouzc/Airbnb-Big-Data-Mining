860c6725a460770d4ee2d13e80457772ee0b5d48
Brian Z <brianzhouzc@gmail.com>
2023-11-22 12:39:16 -0800

    Added script to convert pdf to csv

---
+++ source/etl/rebgv/pdf_to_csv.py
@@ -0,0 +1,14 @@
+from nanonets import NANONETSOCR
+import glob
+
+model = NANONETSOCR()
+model.set_token('6dbb97df-896c-11ee-a76a-12e83ea2166a') # https://app.nanonets.com/#/keys
+
+directory = "../../../datasets"
+pdfs = glob.glob("%s/rebgv/pdf/trimed/*" % directory)
+
+for raw_pdf in pdfs:
+    print("Processing %s", raw_pdf.split('/')[-1])
+    
+    output_name = raw_pdf.split('/')[-1].split('.')[0]
+    model.convert_to_csv(raw_pdf, output_file_name = '%s/rebgv/csv/raw/%s.csv' % (directory, output_name))
